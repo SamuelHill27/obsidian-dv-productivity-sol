d9cab279a46d5becfd2f3bdf36b2ae199bfd1c02
SamuelHill27 <samuelshill04@gmail.com>
2025-09-21 22:35:36 +0100

    Fix broken sort and add sort by time

--- dailySchedule.js
+++ dailySchedule.js
@@ -1,5 +1,5 @@
 // Globals
-const today = dv.date(dv.current().file.name);
+const today = dv.date(dv.current().file.name).set({ hour: 23, minute: 59 });
 const taskDirs = [dv.current().file.folder, "Current", "Projects"];
 
 // Functions
@@ -18,6 +18,17 @@ const isGroupedTask = (task) => {
 	return isTaskInGroup && !taskIsGroup;
 }
 
+const addDueTime = (task) => {
+	if (!task.dueTime) {
+		task.dueTime = "23:59"; // Default due time if none is set
+	}
+
+	task.due = task.due.set({
+		hour: Number(task.dueTime.split(":")[0]),
+		minute: Number(task.dueTime.split(":")[1])
+	});
+}
+
 const displayTasks = (headerText, tasks) => {
 	if (tasks.length > 0) {
 		dv.header(2, headerText)
@@ -28,20 +39,27 @@ const displayTasks = (headerText, tasks) => {
 }
 
 // Main
-const tasks = dv.pages(`"${taskDirs.join('" or "')}"`).file.tasks
-	.where(p => !p.completed && p.due)
-	.filter(p => !isGroupedTask(p))
-	.sort(p => p.due, 'asc')
-	.sort(p => p.dueTime, `asc`);
+let tasks = dv.pages(`"${taskDirs.join('" or "')}"`).file.tasks
+	.where(t => !t.completed && t.due)
+	.filter(t => !isGroupedTask(t))
+tasks.forEach(addDueTime);
 
-displayTasks("Today", tasks.where(task => task.due <= today)) ? null : dv.paragraph("No tasks today. Yay!");
+const isTasksToday = displayTasks("Today", tasks
+	.where(task => task.due <= today)
+	.sort(t => t.due, "asc"));
+
+isTasksToday? null : dv.paragraph("No tasks today. Yay!");
 
 const yesterday = today.minus({ days: 1 }).toFormat("yyyy-MM-dd");
 dv.span(`<-- [[${yesterday}|Yesterday]]`);
 
-const isTasksTomorrow = displayTasks("Tomorrow", tasks.where(task => task.due > today && task.due <= today.plus({ days: 1 })));
+const isTasksTomorrow = displayTasks("Tomorrow", tasks
+	.where(task => task.due > today && task.due <= today.plus({ days: 1 }))
+	.sort(t => t.due, "asc"));
 
-const isTasksThisWeek = displayTasks("This Week", tasks.where(task => task.due > today.plus({ days: 1 }) && task.due <= today.plus({ days: 7 })));
+const isTasksThisWeek = displayTasks("This Week", tasks
+	.where(task => task.due > today.plus({ days: 1 }) && task.due <= today.plus({ days: 7 }))
+	.sort(t => t.due, "asc"));
 
 isTasksTomorrow || isTasksThisWeek ? null : dv.paragraph("No tasks this week. Yay!");
 
