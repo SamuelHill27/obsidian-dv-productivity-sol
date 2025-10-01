b3f54e1d6b313b7e3bf862a10f5ab8711b456c3f
SamuelHill27 <samuelshill04@gmail.com>
2025-10-01 11:10:25 +0100

    Only get pages in current folder and don't include outlinks to MoCs

--- mocValidator.js
+++ mocValidator.js
@@ -13,11 +13,13 @@ const display = (contentNames, contentType) => {
 // Main
 // get all outlink names
 const bulletedListItems = dv.current().file.lists.filter(l => !l.task);
-const contentOutlinks = bulletedListItems.map(bli => bli.outlinks).filter(ol => ol.length > 0); // only keep items with outlinks
+const contentOutlinks = bulletedListItems.map(bli => bli.outlinks)
+	.filter(ol => ol.length > 0) // only keep items with outlinks
+	.filter(ol => ol[0].display == ol[0].path); // don't include MoCs
 const contentOutlinksNames = contentOutlinks.map(col => col[0].display);
 
 // get all page names
-const contentPages = dv.pages(`"${dv.current().file.folder}"`).file;
+const contentPages = dv.pages(`"${dv.current().file.folder}"`).file.filter(p => p.folder == dv.current().file.folder);
 const contentPagesNames = contentPages.map(cp => cp.name).filter(cpn => cpn != dv.current().file.name);
 
 // orphaned
